e25de97dbc6f6c2154ea9bca3a7c80e899c76f42
Ruchi Sanjay Pawar <ruchipawar96@gmail.com>
2023-04-15 11:54:53 -0400

    Update app_py_2.py

--- app_py_2.py
+++ app_py_2.py
@@ -20,7 +20,16 @@ with open('./my_model.pickle', 'rb') as f:
     model = pickle.load(f)
 
 
-
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 
 primaryColor="#6eb52f"
 backgroundColor="#f0f0f5"
